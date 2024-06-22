d4fc93d883bb1271d47d4a5aa70ad48a3a1e3b00
Sahil Verma <laptopsahil123@gmail.com>
2024-06-23 03:50:12 +0530

    profile page add && jwt token se name nahi get ho raha

--- controller/pageserver.js
+++ controller/pageserver.js
@@ -7,14 +7,15 @@ const dashboardPage = async (req, res) => {
         const token = req.cookies.token;
         if(!token) return res.status(404).send("Session not found");
 
-        const tokenData = jwt.verify(token,process.env.JWT_SECRET);
-
+        const tokenData = jwt.verify(token,process.env.JWT_SECRET,(error)=>{
+            console.log(error);
+        });
+        console.log(tokenData)
         const user = await User.findOne({name : tokenData.name});
         if(!user) return res.status(404).send("user not found");
 
         const posts = await Post.find().populate({
             path: "user",
-            select: "name imgAddress"
         });
 
         res.render("dashboard", { posts: posts, user: user });
@@ -36,13 +37,37 @@ const signupPage = (req, res) => {
     res.render("signup");
 };
 
+const homepage = (req, res) => {
+    res.render("signup");
+};
+
 const postCreatingPage = (req, res) => {
     res.render("postcreationpage");
 };
+
+const profilepage = async (req,res)=>{
+    try { 
+        const uid = req.params.id;
+        console.log(uid)
+        const user = await User.findOne({
+            _id : uid,
+        }).populate({
+            path : 'posts',
+        })
+        if(!user) return res.status(500).send("internal server error")
+        
+        res.render("profile",{user : user});
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("internal server error");
+    }
+}
 
 module.exports = {
     dashboardPage,
     loginPage,
     signupPage,
     postCreatingPage,
+    homepage,
+    profilepage,
 };
--- route/pageServing.js
+++ route/pageServing.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
-const { loginPage, signupPage, dashboardPage, postCreatingPage } = require("./../controller/pageserver")
+const { loginPage, signupPage, dashboardPage, postCreatingPage, homepage, profilepage } = require("./../controller/pageserver")
 
 const route = express.Router();
 
+route.get("/", homepage);
 route.get("/login", loginPage);
 route.get("/signup", signupPage);
+route.get("/profile/:id",profilepage);
 route.get("/dashboard", dashboardPage);
 route.get("/createpost", postCreatingPage);
 
